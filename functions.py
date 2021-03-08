8bde3953d05b92e31e50982529e434344b953736
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2021-03-08 11:52:22 -0500

    Added features
    Added capabilities:
    -SSID Firewall and Traffic Shaping
    -Switch QoS and STP
    -Network Alerts, SNMP, Syslog, Traffic Analytics and Group Policies

---
+++ functions.py
@@ -0,0 +1,176 @@
+import meraki
+import credentials
+
+def ssid_firewall(dashboard, src_temp_id, dst_temp_id, dst_org_id):
+    actions = []
+    for n in range(15):
+        l3_fw = dashboard.wireless.getNetworkWirelessSsidFirewallL3FirewallRules(networkId=src_temp_id, number=n)
+        l7_fw = dashboard.wireless.getNetworkWirelessSsidFirewallL7FirewallRules(networkId=src_temp_id, number=n)
+
+        indices = []
+        for i in range(len(l3_fw['rules'])):
+            if l3_fw['rules'][i]['comment'] == 'Wireless clients accessing LAN':
+                indices.append(i)
+            elif l3_fw['rules'][i]['comment'] == 'Default rule':
+                indices.append(i)
+        for item in sorted(indices, reverse=True):
+            l3_fw['rules'].pop(item)
+
+        a = {
+            "resource": f"/networks/{dst_temp_id}/wireless/ssids/{n}/firewall/l7FirewallRules",
+            "operation": "update",
+            "body": {
+                **l7_fw
+            }
+        }
+        actions.append(a)
+        dashboard.wireless.updateNetworkWirelessSsidFirewallL3FirewallRules(networkId=dst_temp_id, number=n, **l3_fw)
+
+    dashboard.organizations.createOrganizationActionBatch(organizationId=dst_org_id, actions=actions,
+                                                                     confirmed=True)
+
+def ssid_shaping(dashboard, src_temp_id, dst_temp_id, dst_org_id):
+    actions = []
+    for i in range(15):
+        shaping = dashboard.wireless.getNetworkWirelessSsidTrafficShapingRules(networkId=src_temp_id, number=i)
+        a = {
+            "resource": f"/networks/{dst_temp_id}/wireless/ssids/{i}/trafficShaping/rules",
+            "operation": "update",
+            "body": {
+                **shaping
+            }
+        }
+        actions.append(a)
+
+    dashboard.organizations.createOrganizationActionBatch(organizationId=dst_org_id, actions=actions, confirmed=True)
+
+def switch_qos(dashboard, src_temp_id, dst_temp_id, dst_org_id):
+    src_qos = dashboard.switch.getNetworkSwitchQosRules(networkId=src_temp_id)
+    dst_qos_order = dashboard.switch.getNetworkSwitchQosRulesOrder(networkId=dst_temp_id)
+    actions = []
+    # Delete all QoS rules in target template
+    for item in dst_qos_order['ruleIds']:
+        a = {
+            "resource": f"/networks/{dst_temp_id}/switch/qosRules/{item}",
+            "operation": "destroy",
+            "body": {}
+        }
+        actions.append(a)
+    # Create new QoS rules in target template matching source template
+    for item in src_qos:
+        # Remove source port and destination port from payload if set to None/Nay
+        # Handle exception where source/destination is a range, not individual port
+        try:
+            if item['srcPort'] == None and item['protocol'] != 'ANY':
+                del item['srcPort']
+        except KeyError:
+            print('Source port is a range')
+            pass
+        try:
+            if item['dstPort'] == None and item['protocol'] != 'ANY':
+                del item['dstPort']
+        except KeyError:
+            print('Destination port is a range')
+            pass
+
+        a = {
+            "resource": f"/networks/{dst_temp_id}/switch/qosRules",
+            "operation": "create",
+            "body": {
+                **item
+            }
+        }
+        actions.append(a)
+
+    # Split actions in chunks of 20 to send synchronous batches and keep QoS Rule order
+    batches = [actions[x:x + 20] for x in range(0, len(actions), 20)]
+
+    # Create one synchronous action batch for every batch in batches
+    for batch in batches:
+        dashboard.organizations.createOrganizationActionBatch(organizationId=dst_org_id, actions=batch, confirmed=True,
+                                                              synchronous=True)
+def switch_stp(dashboard, src_temp_id, src_org_id, dst_temp_id, dst_org_id):
+    src_switch_profiles = dashboard.switch.getOrganizationConfigTemplateSwitchProfiles(
+        organizationId=src_org_id, configTemplateId=src_temp_id)
+    dst_switch_profiles = dashboard.switch.getOrganizationConfigTemplateSwitchProfiles(
+        organizationId=dst_org_id, configTemplateId=dst_temp_id)
+
+    src_switch_set = [item['model'] for item in src_switch_profiles]
+    dst_switch_set = [item['model'] for item in dst_switch_profiles]
+
+    if src_switch_set == dst_switch_set:
+        src_stp = dashboard.switch.getNetworkSwitchStp(networkId=src_temp_id)
+        payload = src_stp
+        dst_stp = dashboard.switch.getNetworkSwitchStp(networkId=dst_temp_id)
+        for i in range(len(src_stp['stpBridgePriority'])):
+            for n in range(len(src_stp['stpBridgePriority'][i]['switchProfiles'])):
+                for src_profile in src_switch_profiles:
+                    if src_stp['stpBridgePriority'][i]['switchProfiles'][n] == src_profile['switchProfileId']:
+                        for dst_profile in dst_switch_profiles:
+                            if src_profile['model'] == dst_profile['model']:
+                                src_stp['stpBridgePriority'][i]['switchProfiles'][n] = dst_profile['switchProfileId']
+        dashboard.switch.updateNetworkSwitchStp(networkId=dst_temp_id, **src_stp)
+
+def group_policies(dashboard, src_temp_id, dst_temp_id, dst_org_id):
+    src_policies = dashboard.networks.getNetworkGroupPolicies(networkId=src_temp_id)
+    dst_policies = dashboard.networks.getNetworkGroupPolicies(networkId=dst_temp_id)
+
+    actions = []
+    for policy in dst_policies:
+        a = {
+            "resource": f"/networks/{dst_temp_id}/groupPolicies/{policy['groupPolicyId']}",
+            "operation": "destroy",
+            "body": {}
+        }
+        actions.append(a)
+
+    for policy in src_policies:
+        dict = policy
+        # Remove unneeded key from dict
+        del dict['groupPolicyId']
+        a = {
+            "resource": f"/networks/{dst_temp_id}/groupPolicies",
+            "operation": "create",
+            "body": {
+                **dict
+            }
+        }
+        actions.append(a)
+
+    # Split actions in chunks of 20 to send synchronous batches and destroy existing policies before creating new ones
+    batches = [actions[x:x + 20] for x in range(0, len(actions), 20)]
+
+    # Create one synchronous action batch for every batch in batches
+    for batch in batches:
+        dashboard.organizations.createOrganizationActionBatch(organizationId=dst_org_id, actions=batch, confirmed=True,
+                                                              synchronous=True)
+
+def net_alerts(dashboard, src_temp_id, dst_temp_id):
+    src_alerts = dashboard.networks.getNetworkAlertsSettings(networkId=src_temp_id)
+    dst_alerts = dashboard.networks.getNetworkAlertsSettings(networkId=dst_temp_id)
+
+    for i in range(len(dst_alerts['alerts'])):
+        for src_alert in src_alerts['alerts']:
+            if dst_alerts['alerts'][i]['type'] == src_alert['type']:
+                if src_alert != dst_alerts['alerts'][i]:
+                    dst_alerts['alerts'][i] = src_alert
+
+    # Remove clientConnectivity alerts, as they give issues when updating
+    for i in range(len(dst_alerts['alerts'])):
+        if dst_alerts['alerts'][i]['type'] == 'clientConnectivity':
+            p = i
+
+    dst_alerts['alerts'].pop(p)
+    dashboard.networks.updateNetworkAlertsSettings(networkId=dst_temp_id, **dst_alerts)
+
+def net_syslog(dashboard, src_temp_id, dst_temp_id):
+    src_syslog = dashboard.networks.getNetworkSyslogServers(networkId=src_temp_id)
+    dashboard.networks.updateNetworkSyslogServers(networkId=dst_temp_id, servers=src_syslog['servers'])
+
+def net_snmp(dashboard, src_temp_id, dst_temp_id):
+    src_snmp = dashboard.networks.getNetworkSnmp(networkId=src_temp_id)
+    dashboard.networks.updateNetworkSnmp(networkId=dst_temp_id, **src_snmp)
+
+def net_analytics(dashboard, src_temp_id, dst_temp_id):
+    src_ta = dashboard.networks.getNetworkTrafficAnalysis(networkId=src_temp_id)
+    dashboard.networks.updateNetworkTrafficAnalysis(networkId=dst_temp_id, **src_ta)
---
+++ main.py
@@ -0,0 +1,255 @@
+import meraki
+import PySimpleGUI as sg
+import credentials
+import functions
+
+def gather_templates(api_client):
+
+    orgs = api_client.organizations.getOrganizations()
+    temps = []
+
+    for org in orgs:
+        temp = dashboard.organizations.getOrganizationConfigTemplates(organizationId=org['id'])
+        temps.append(temp)
+    org_templates = []
+
+    for i in range(len(orgs)):
+        dict = orgs[i]
+        dict['templates'] = temps[i]
+        org_templates.append(dict)
+
+    return org_templates
+
+def open_window(message, lines=4, width=20):
+    layout = [
+        [sg.Text(
+            f'{message}',
+            size=(width, lines), font=('Any', 16), text_color='#1c86ee',
+            justification='center')],
+        # [sg.Image(data=imgbytes, key='_IMAGE_')],
+        [sg.Exit()]
+    ]
+    win = sg.Window('Meraki Cross-Org Templates',
+                    default_element_size=(14, 1),
+                    text_justification='right',
+                    auto_size_text=False,
+                    font=('Helvetica', 20)).Layout(layout)
+    win.Read()
+
+if __name__ == "__main__":
+    dashboard = meraki.DashboardAPI(api_key=credentials.api_key)
+    org_templates = gather_templates(dashboard)
+    org_temp_list = []
+    for org in org_templates:
+        for temp in org['templates']:
+            item = {'name': org['name']+' - '+ temp['name'], 'id':temp['id'], 'org_id': org['id'], 'prodTypes':temp['productTypes']}
+            org_temp_list.append(item)
+
+    sg.ChangeLookAndFeel('LightGreen')
+    layout = [
+        [sg.Text('Meraki Cross Org Templates', size=(19, 1), font=('Any', 18), text_color='#1c86ee',
+                 justification='center')],
+        [sg.Text('Source Template  '), sg.Combo([temp['name'] for temp in org_temp_list], key='_SRC_TMP_')],
+        [sg.Text('Target Template  '), sg.Combo([temp['name'] for temp in org_temp_list], key='_DST_TMP_')],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Wireless - SSIDs', key='_SSID_', size=(40,1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Wireless - SSID Firewall', key='_FW_', size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Wireless - SSID Traffic Shaping', key="_SHAPE_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Switch - Ports and Profiles', key="_PORT_PRF_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Switch - QoS Settings', key="_QOS_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Switch - STP Settings', key="_STP_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Network - Group Policies', key="_GP_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Network - Alerts', key="_ALERT_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Network - Syslog', key="_SYSLOG_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Network - SNMP', key="_SNMP_", size=(40, 1))],
+        [sg.Text(' ' * 8), sg.Checkbox('Clone Network - Traffic Analysis', key="_ANALYTICS_", size=(40, 1))],
+        [sg.OK(), sg.Cancel()]
+    ]
+
+    win = sg.Window('Meraki Cross Org Templates',
+                    default_element_size=(20, 1),
+                    text_justification='right',
+                    auto_size_text=False,
+                    font=('Helvetica', 20)).Layout(layout)
+    event, values = win.Read()
+    if event is None or event == 'Cancel':
+        exit()
+    src_temp = values['_SRC_TMP_']
+    dst_temp = values['_DST_TMP_']
+    clone_ssids = values['_SSID_']
+    clone_switch_profiles = values['_PORT_PRF_']
+    clone_ssid_fw = values['_FW_']
+    clone_ssid_ts = values['_SHAPE_']
+    clone_switch_qos = values['_QOS_']
+    clone_switch_stp = values['_STP_']
+    clone_net_gps = values['_GP_']
+    clone_net_alerts = values['_ALERT_']
+    clone_net_syslog = values['_SYSLOG_']
+    clone_net_snmp = values['_SNMP_']
+    clone_net_ta = values['_ANALYTICS_']
+    args = values
+
+    win.Close()
+
+    for i in range(len(org_temp_list)):
+        if org_temp_list[i]['name']==src_temp:
+            src_temp_id = org_temp_list[i]['id']
+            src_org_id = org_temp_list[i]['org_id']
+            src_temp_prods = org_temp_list[i]['prodTypes']
+        if org_temp_list[i]['name']==dst_temp:
+            dst_temp_id = org_temp_list[i]['id']
+            dst_org_id = org_temp_list[i]['org_id']
+            dst_temp_prods = org_temp_list[i]['prodTypes']
+
+    win_started = False
+    try:
+        if clone_ssids:
+            if 'wireless' not in src_temp_prods:
+                error = 'Your source template does not have wireless products, so you cannot copy SSIDs from it.'
+                open_window(error)
+            elif 'wireless' not in dst_temp_prods:
+                error = 'Your destination template does not have wireless products, so you cannot copy SSIDs to it.'
+                open_window(error)
+            else:
+                try:
+                    ssids = dashboard.wireless.getNetworkWirelessSsids(networkId=src_temp_id)
+                    actions = []
+                    for ssid in ssids:
+                        d = ssid
+                        upd = dict(d)
+                        del upd['number']
+                        a = {
+                            "resource": f"/networks/{dst_temp_id}/wireless/ssids/{d['number']}",
+                            "operation": "update",
+                            "body": {
+                                **upd
+                            }
+                        }
+                        actions.append(a)
+                    batch = dashboard.organizations.createOrganizationActionBatch(organizationId=dst_org_id, actions=actions, confirmed=True)
+                except meraki.APIError as e:
+                    open_window(e)
+        if clone_switch_profiles:
+            if 'switch' not in src_temp_prods:
+                error = 'Your source template does not have switch products, so you cannot copy ports from it.'
+                open_window(error)
+            elif 'switch' not in dst_temp_prods:
+                error = 'Your destination template does not have switch products, so you cannot copy ports to it.'
+                open_window(error)
+            else:
+                try:
+                    src_switch_profiles = dashboard.switch.getOrganizationConfigTemplateSwitchProfiles(
+                        organizationId=src_org_id, configTemplateId=src_temp_id)
+                    dst_switch_profiles = dashboard.switch.getOrganizationConfigTemplateSwitchProfiles(
+                        organizationId=dst_org_id, configTemplateId=dst_temp_id)
+
+                    src_switch_set = [item['model'] for item in src_switch_profiles]
+                    dst_switch_set = [item['model'] for item in dst_switch_profiles]
+
+                    if src_switch_set == dst_switch_set:
+                        for profile in src_switch_profiles:
+                            switch_profiles_ports = dashboard.switch.getOrganizationConfigTemplateSwitchProfilePorts(
+                                organizationId=src_org_id, configTemplateId=src_temp_id,
+                                profileId=profile['switchProfileId'])
+                            for dst_profile in dst_switch_profiles:
+                                if dst_profile['model']==profile['model']:
+                                    actions = []
+                                    for port in switch_profiles_ports:
+                                        d = port
+                                        upd = dict(d)
+                                        del upd['portId']
+                                        a = {
+                                            "resource": f"/organizations/{dst_org_id}/configTemplates/{dst_temp_id}/switch/profiles/{dst_profile['switchProfileId']}/ports/{port['portId']}",
+                                            "operation": "update",
+                                            "body": {
+                                                **upd
+                                            }
+                                        }
+                                        actions.append(a)
+                                    batch = dashboard.organizations.createOrganizationActionBatch(organizationId=dst_org_id,actions=actions, confirmed=True)
+
+                    else:
+                        error = f'Your source template has profiles for {src_switch_set}'
+                        error = error + f'\nYour destination template has profiles for {dst_switch_set}'
+                        error = error + '\n\nMake sure they are the same before running the config sync tool.'
+                        print(error)
+                        open_window(error,lines=8,width=45)
+                except meraki.APIError as e:
+                    open_window(e)
+        if clone_ssid_fw:
+            if 'wireless' not in src_temp_prods:
+                error = 'Your source template does not have wireless products, so you cannot copy SSIDs from it.'
+                open_window(error)
+            elif 'wireless' not in dst_temp_prods:
+                error = 'Your destination template does not have wireless products, so you cannot copy SSIDs to it.'
+                open_window(error)
+            else:
+                try:
+                    functions.ssid_firewall(dashboard=dashboard, src_temp_id=src_temp_id,dst_temp_id=dst_temp_id,dst_org_id=dst_org_id)
+                except meraki.APIError as e:
+                    open_window(e)
+        if clone_ssid_ts:
+            if 'wireless' not in src_temp_prods:
+                error = 'Your source template does not have wireless products, so you cannot copy SSIDs from it.'
+                open_window(error)
+            elif 'wireless' not in dst_temp_prods:
+                error = 'Your destination template does not have wireless products, so you cannot copy SSIDs to it.'
+                open_window(error)
+            else:
+                try:
+                    functions.ssid_shaping(dashboard=dashboard, src_temp_id=src_temp_id, dst_org_id=dst_org_id, dst_temp_id=dst_temp_id)
+                except meraki.APIError as e:
+                    open_window(e)
+        if clone_switch_qos:
+            if 'switch' not in src_temp_prods:
+                error = 'Your source template does not have switch products, so you cannot copy ports from it.'
+                open_window(error)
+            elif 'switch' not in dst_temp_prods:
+                error = 'Your destination template does not have switch products, so you cannot copy ports to it.'
+                open_window(error)
+            else:
+                try:
+                    functions.switch_qos(dashboard=dashboard, src_temp_id=src_temp_id, dst_temp_id=dst_temp_id,dst_org_id=dst_org_id)
+                except meraki.APIError as e:
+                    open_window(e)
+        if clone_switch_stp:
+            if 'switch' not in src_temp_prods:
+                error = 'Your source template does not have switch products, so you cannot copy ports from it.'
+                open_window(error)
+            elif 'switch' not in dst_temp_prods:
+                error = 'Your destination template does not have switch products, so you cannot copy ports to it.'
+                open_window(error)
+            else:
+                try:
+                    functions.switch_stp(dashboard=dashboard, src_temp_id=src_temp_id, src_org_id=src_org_id, dst_temp_id=dst_temp_id, dst_org_id=dst_org_id)
+                except meraki.APIError as e:
+                    open_window(e)
+        if clone_net_gps:
+            try:
+                functions.group_policies(dashboard=dashboard,src_temp_id=src_temp_id,dst_temp_id=dst_temp_id, dst_org_id=dst_org_id)
+            except meraki.APIError as e:
+                open_window(e)
+        if clone_net_alerts:
+            try:
+                functions.net_alerts(dashboard=dashboard, src_temp_id=src_temp_id, dst_temp_id=dst_temp_id)
+            except meraki.APIError as e:
+                open_window(e)
+        if clone_net_syslog:
+            try:
+                functions.net_syslog(dashboard=dashboard, src_temp_id=src_temp_id, dst_temp_id=dst_temp_id)
+            except meraki.APIError as e:
+                open_window(e)
+        if clone_net_snmp:
+            try:
+                functions.net_snmp(dashboard=dashboard, src_temp_id=src_temp_id, dst_temp_id=dst_temp_id)
+            except meraki.APIError as e:
+                open_window(e)
+        if clone_net_ta:
+            try:
+                functions.net_analytics(dashboard=dashboard, src_temp_id=src_temp_id, dst_temp_id=dst_temp_id)
+            except meraki.APIError as e:
+                open_window(e)
+
+        success = 'Your template was copied successfully!'
+        open_window(success)
+    except meraki.APIError as e:
+        open_window(e)
